410a34c35384beedd1f546c725785deb0d544348
Blue751 <146474250+Blue751@users.noreply.github.com>
2023-09-30 14:15:32 -0400

    Update Footer.tsx

--- client/src/components/Footer/Footer.tsx
+++ client/src/components/Footer/Footer.tsx
@@ -1,7 +1,7 @@
 import "./Footer.style.scss";
 
 function Footer() {
-  return <div className="footer"><center>© 2023 Carleton University</center></div>;
+  return <div className="footer">© 2023 Carleton University</div>;
 }
 
 export default Footer;
